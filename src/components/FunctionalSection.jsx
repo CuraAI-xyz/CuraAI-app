2749a711194f5e50579bf4071b2592913b8a7c8b
mauroradino <mauroradino2003@gmail.com>
2025-10-19 14:06:37 -0300

    toggle button agent

--- src/components/FunctionalSection.jsx
+++ src/components/FunctionalSection.jsx
@@ -6,7 +6,10 @@ function FunctionalSection (){
         <section className="w-full flex flex-col justify-center gap-8">
             <RecordingButton/>
             <div className="border-1 rounded-lg mx-auto h-80 w-6/12 border-gray-200 shadow-sm overflow-y-auto">
-            <p className="flex justify-between bg-gray-100 p-4 font-bold text-xl text-gray-600 rounded-t-lg">Conversation <ToggleButton/></p>
+            <div className="flex justify-between bg-gray-100 p-4 font-bold text-xl text-gray-600 rounded-t-lg">
+            <p>Conversation </p>
+            <ToggleButton/>
+            </div>
             </div>
         </section>
     )
--- src/components/ToggleButton.jsx
+++ src/components/ToggleButton.jsx
@@ -3,11 +3,32 @@ import { useState } from "react";
 export default function ToggleButton() {
   const [active, setActive] = useState(false);
 
+  const handleToggle = async () => {
+    setActive(!active);
+
+    const url = active
+      ? "http://127.0.0.1:8000/stop_agent"
+      : "http://127.0.0.1:8000/start_agent";
+
+    try {
+      const res = await fetch(url);
+      if (!res.ok) throw new Error("Error " + res.status);
+      const data = await res.json();
+      console.log("Respuesta:", data);
+    } catch (err) {
+      console.error("Error:", err);
+    }
+  };
+
   return (
     <div
-      onClick={() => setActive(!active)}
-      className={`w-14 h-8 flex items-center rounded-full p-1 cursor-pointer transition-all ${active ? "bg-[#61A5C2]" : "bg-gray-300"}`}>
-      <div className={`bg-white w-6 h-6 rounded-full shadow-md transform transition-transform ${
+      onClick={handleToggle}
+      className={`w-14 h-8 flex items-center rounded-full p-1 cursor-pointer transition-all ${
+        active ? "bg-[#61A5C2]" : "bg-gray-300"
+      }`}
+    >
+      <div
+        className={`bg-white w-6 h-6 rounded-full shadow-md transform transition-transform ${
           active ? "translate-x-6" : ""
         }`}
       ></div>
